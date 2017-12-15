d0ae439c9eaffe5419fe25df2efbd97aae158e27
zackad <zackad.dev@gmail.com>
2017-12-15 21:55:58 +0700

    add NormalizeCoordinate class file

---
+++ src/NormalizeCoordinate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zackad\GIS;
+
+/**
+* A simple library to normalize geographic coordinate into valid range
+* -180 < longitude < 180
+* -90  < latitude  < 90
+*/
+class NormalizeCoordinate
+{
+
+}
